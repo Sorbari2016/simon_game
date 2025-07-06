c7f4b1cd02526669f8a5fc59804eda5a3c740552
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-07-06 21:02:04 +0100

    Created a function to generate random colors

--- game.js
+++ game.js
@@ -1,2 +1,20 @@
 // SCRIPT CODE 
- 
+// Create an empty array
+const gamePattern = []; 
+
+// Array to hold button colors 
+const buttonColours = [
+    "red", 
+    "blue", 
+    "green", 
+    "yellow"
+]
+
+
+// Generate a random color.
+function nextSequence() {
+    const NUMBER = 3;
+    const randomNumber = Math.floor(Math.random() * (NUMBER + 1)); 
+    const randomChosenColour = buttonColours[randomNumber]; 
+    return gamePattern.push(randomChosenColour); 
+} 
