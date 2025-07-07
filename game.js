78d38a6c8f70b7a7d0da1bd83d1eaa6f8c7428a1
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-07-07 13:38:31 +0100

    Used the random color to select the buttons

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-// SCRIPT CODE 
+// SCRIPT CODE WITH JQUERY
+
 // Create an empty array
 const gamePattern = []; 
 
@@ -11,10 +12,24 @@ const buttonColours = [
 ]
 
 
+
+
 // Generate a random color.
 function nextSequence() {
     const NUMBER = 3;
     const randomNumber = Math.floor(Math.random() * (NUMBER + 1)); 
     const randomChosenColour = buttonColours[randomNumber]; 
-    return gamePattern.push(randomChosenColour); 
+    
+    // Use the color generated to select the button with the same id, Add flash
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    let audio = new Audio("../assets/sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+
+
+    gamePattern.push(randomChosenColour);  
 } 
+
+nextSequence(); 
+
+
+
